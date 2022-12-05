cd87c8d4ed25430278cf2308a501191cfc109b23
Tomoya Shikanai <tomoya.shikanai@gmail.com>
2022-12-05 19:17:19 +0900

    add led turn on/off program.

---
+++ Leafony/led.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+}
